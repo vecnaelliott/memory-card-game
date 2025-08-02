31cb491d406b45e6b33052aefa3dee81aaa7ccfd
vecnaelliott <erkanyrlsan@gmail.com>
2025-08-02 03:41:50 +0300

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+export default function App() {
+  return (
+    <div style={{
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      minHeight: "100vh",
+      background: "#111",
+      color: "#fff",
+      fontFamily: "sans-serif"
+    }}>
+      <h1>React + Vite versiyonu çalıştı 🎉</h1>
+    </div>
+  );
+}
